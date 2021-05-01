87399c410da3e44605a2e813c541aa6be9f9cb7d
Rishabh <rishabh@rhatechnology.com>
2021-05-01 20:13:33 +0530

    Auth Service: setup & impl. custom errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = Router();
 
@@ -16,13 +18,14 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password");
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     console.log("Create:", { email, password });
-    throw new Error("Error connecting to Database");
+
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
