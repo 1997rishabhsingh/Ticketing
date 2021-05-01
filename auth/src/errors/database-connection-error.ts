daca1a1a7a18eca899a677ec841044851659ab6c
Rishabh <rishabh@rhatechnology.com>
2021-05-01 20:25:12 +0530

    Auth Service: improvise custom errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = "Error connecting to database";
 
   constructor() {
     super();
     // NOTE: Required when extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeError() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,16 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super();
 
     // NOTE: Required when extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeError() {
+    return this.errors.map((e) => ({ message: e.msg, field: e.param }));
   }
 }
