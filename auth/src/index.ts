84d46a5ee6914379ef5e5b787757474b3bd970c3
Rishabh <rishabh@rhatechnology.com>
2021-05-04 16:22:33 +0530

    Auth Service: send jwt on sign up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 require("express-async-errors");
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
@@ -10,9 +11,16 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 
 // middlewares
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 // route handlers
 app.use(currentUserRouter);
