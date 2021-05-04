346600bc4f444bc4b724e0bb343b3741fddb5b21
Rishabh <rishabh@rhatechnology.com>
2021-05-04 18:27:11 +0530

    Auth Service: impl. signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Sign In");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Please provide password")
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const match = await Password.compare(existingUser.password, password);
+
+    if (!match) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store jwt in session
+    req.session = { jwt: userJwt };
+
+    res.status(200).send(existingUser);
+
+    res.send({});
+  }
+);
 
 export { router as signinRouter };
