80da6fbb3c6750e26d1f459c8e11f54e82e0383c
Rishabh <rishabh@rhatechnology.com>
2021-05-04 21:01:24 +0530

    Auth Service: add & impl. current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,15 @@
 import { Router, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+
+import { currentUser } from "../middlewares/current-user";
 
 const router = Router();
 
-router.get("/api/users/currentuser", async (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  async (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-    res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
