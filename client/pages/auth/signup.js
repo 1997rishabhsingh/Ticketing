4fa8bf25dd2f82f0bc457d2397ba0837e152f0d0
Rishabh <rishabh@rhatechnology.com>
2021-05-09 11:27:03 +0530

    Client: add errors in signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
-import { Form, FormGroup, Input, Button, Label } from "reactstrap";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Label,
+  FormFeedback,
+  Alert
+} from "reactstrap";
 import axios from "axios";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const { data } = await axios({
-      url: "/api/users/signup",
-      method: "POST",
-      data: { email, password }
-    });
-
-    console.log(data);
+    try {
+      await axios({
+        url: "/api/users/signup",
+        method: "POST",
+        data: { email, password }
+      });
+    } catch (err) {
+      const errs = err.response.data.errors.reduce(
+        (acc, e) => ({ ...acc, [e.field]: e.message }),
+        {}
+      );
+      setErrors(errs);
+    }
   };
 
   return (
@@ -26,19 +40,24 @@ const Signup = () => {
           <Label id="email">Email Address</Label>
           <Input
             id="email"
+            invalid={!!errors.email}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <FormFeedback>{errors.email}</FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label id="password">Password</Label>
           <Input
             id="password"
             type="password"
+            invalid={!!errors.password}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <FormFeedback>{errors.password}</FormFeedback>
         </FormGroup>
+
         <Button color="primary" className="mt-3">
           Sign Up
         </Button>
