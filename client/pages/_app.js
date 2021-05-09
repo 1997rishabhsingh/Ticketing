face5f01d3bf2f9a8e8d9f0af94df9d339fb24ff
Rishabh <rishabh@rhatechnology.com>
2021-05-09 19:32:34 +0530

    Client: enable getInitialProps for pages & _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
-import "bootstrap/dist/css/bootstrap.css";
+import App from "next/app";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const { data } = await buildClient(appContext.ctx).get(
+    "/api/users/currentuser"
+  );
+
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps, ...data };
+};
+
+export default AppComponent;
