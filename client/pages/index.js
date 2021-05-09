e739bed640f5f4fed39beba1f3cdb3862c83cf94
Rishabh <rishabh@rhatechnology.com>
2021-05-09 18:47:49 +0530

    Client: SSR setup for index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,15 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return (
-    <div>
-      Landing
-      <h5>Here</h5>
-    </div>
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
   );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  let config = {};
-
-  if (typeof window === "undefined") {
-    // When environment is server Domain format => http://SERVICENAME.NAMESPACE.svc.cluster.local
-    config = {
-      url:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      // Since nginx is configured for this host and on server things are different
-      headers: req.headers
-    };
-  } else {
-    config = {
-      url: "/api/users/currentuser"
-    };
-  }
-
-  config.method = "GET";
-
-  const { data } = await axios(config);
+LandingPage.getInitialProps = async (ctx) => {
+  const { data } = await buildClient(ctx).get("/api/users/currentuser");
 
   return data;
 };
