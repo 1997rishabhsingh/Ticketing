c91df7c7ef2a7c5a47c6388cc0d5c67d778a1987
Rishabh <rishabh@rhatechnology.com>
2021-05-15 14:55:07 +0530

    Ticket Service: add ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,56 @@
+import { Schema, model, Model, Document } from "mongoose";
+
+// inerface to define structure to create new User
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDocument extends Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends Model<TicketDocument> {
+  build(attrs: TicketAttrs): TicketDocument;
+}
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      versionKey: false, // not include __v (could also be done via delete ret.__v)
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+
+        return ret;
+      }
+    }
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = model<TicketDocument, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,4 +1,6 @@
 import request from "supertest";
+import faker from "faker";
+
 import { app } from "../../app";
 
 it("has a POST route /api/tickets", async () => {
@@ -22,8 +24,59 @@ it("returns a ststus other than 401 for logged in user", async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it("returns error if invalid title provided", async () => {});
+it("returns error if invalid title provided", async () => {
+  const { cookie } = global.signin();
 
-it("returns error if invalid price provided", async () => {});
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: faker.commerce.price()
+    })
+    .expect(400);
 
-it("creates a ticket with valid inputs", async () => {});
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      price: faker.commerce.price()
+    })
+    .expect(400);
+});
+
+it("returns error if invalid price provided", async () => {
+  const { cookie } = global.signin();
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: faker.commerce.product(),
+      price: -faker.commerce.price()
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: faker.commerce.product()
+    })
+    .expect(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
+  const { cookie } = global.signin();
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: faker.commerce.product(),
+      price: faker.commerce.price()
+    })
+    .expect(201);
+
+  // check if ticket was saved in db
+});
