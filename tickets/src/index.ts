3d49c341035a841e6a625d4cdefecf5f196f8e55
Rishabh <rishabh@rhatechnology.com>
2021-05-15 11:41:14 +0530

    move mongo uri to depl config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("Environment Variable 'JWT_KEY' not defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("Environment Variable 'MONGO_URI' not defined");
+  }
 
   try {
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
