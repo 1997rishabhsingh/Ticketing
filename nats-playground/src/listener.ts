605c84e849207d9022167abeccd3add524ecdec2
Rishabh <rishabh@rhatechnology.com>
2021-05-18 01:02:23 +0530

    terminate nats clients immediately on exit

--- nats-playground/src/listener.ts
+++ nats-playground/src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
   const subscription = stan.subscribe(
     "ticket:created",
@@ -25,4 +30,18 @@ stan.on("connect", () => {
     }
     msg.ack();
   });
+});
+
+// Won't neccessarily be triggered since it might vary with platform
+process.on("SIGINT", () => {
+  console.log("SIGINT");
+  stan.close();
+});
+process.on("SIGTERM", () => {
+  console.log("SIGTERM");
+  stan.close();
+});
+process.on("SIGKILL", () => {
+  console.log("SIGKILL");
+  stan.close();
 });
