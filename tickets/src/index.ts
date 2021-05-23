ee37083d8207654d81b5a727f4901e989543eb8b
Rishabh <rishabh@rhatechnology.com>
2021-05-23 20:21:22 +0530

    Ticket Service: cleanup nats on termination

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,14 @@ const start = async () => {
 
   try {
     await natsWrapper.connect("ticketing", "dgdsfvfds", "http://nats-srv:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    process.on("SIGINT", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
