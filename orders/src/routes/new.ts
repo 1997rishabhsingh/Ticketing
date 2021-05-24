ce4aae04d2554d378bc6d4325f6855f7d3b9029f
Rishabh <rishabh@rhatechnology.com>
2021-05-24 20:20:50 +0530

    Orders Service: validation on create order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
 import { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@rishtickets/common";
 
 const router = Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [body("ticketId").not().isEmpty().withMessage("Ticket Id is required")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
