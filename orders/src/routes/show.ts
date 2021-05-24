f130640e5b35782fcc7d8f263ca92a33fffbee51
Rishabh <rishabh@rhatechnology.com>
2021-05-25 00:10:55 +0530

    Orders Service: add route for get order by id

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,26 @@
+import { NotFoundError, requireAuth } from "@rishtickets/common";
 import { Request, Response, Router } from "express";
+import { Order } from "../models/order";
 
 const router = Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findOne({
+      id: orderId,
+      userId: req.currentUser!.id
+    }).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
