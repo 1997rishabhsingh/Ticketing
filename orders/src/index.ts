964f011170b4ed0ebef903063ceab248acf9916e
Rishabh <rishabh@rhatechnology.com>
2021-06-01 12:18:37 +0530

    Orders Service: update

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -25,6 +25,8 @@ const start = async () => {
   }
 
   try {
+    console.log("Starting...");
+
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
